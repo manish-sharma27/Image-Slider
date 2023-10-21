1629646107663aeff0f462cd8f61a2876ac9cb59
Manish Sharma <64796718+manish-sharma27@users.noreply.github.com>
2023-10-21 15:39:56 +0530

    Update Slider.jsx

--- src/Slider.jsx
+++ src/Slider.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import image1 from './images/image1.jpg'
 import img2 from './images/img2.jpg'
 import img3 from './images/img3.jpg'
@@ -8,40 +8,28 @@ const images  = [
   img3
   
 ];
+const ImageSlider = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-class ImageSlider extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentIndex: 0,
-    };
-  }
-
-  nextSlide = () => {
-    this.setState((prevState) => ({
-      currentIndex: (prevState.currentIndex + 1) % images.length,
-    }));
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
   };
 
-  prevSlide = () => {
-    this.setState((prevState) => ({
-      currentIndex:
-        (prevState.currentIndex - 1 + images.length) % images.length,
-    }));
-  };
-
-  render() {
-    return (
-      <div className="image-slider">
-        <button onClick={this.prevSlide}>Previous</button>
-        <img
-          src={images[this.state.currentIndex]}
-          alt={`Image ${this.state.currentIndex + 1}`}
-        />
-        <button onClick={this.nextSlide}>Next</button>
-      </div>
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      (prevIndex - 1 + images.length) % images.length
     );
-  }
-}
+  };
 
+  return (
+    <div className="image-slider">
+      <button onClick={prevSlide}>Previous</button>
+      <img
+        src={images[currentIndex]}
+        alt={`Image ${currentIndex + 1}`}
+      />
+      <button onClick={nextSlide}>Next</button>
+    </div>
+  );
+};
 export default ImageSlider;
